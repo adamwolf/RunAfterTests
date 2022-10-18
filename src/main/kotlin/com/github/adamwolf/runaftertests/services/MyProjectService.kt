79cc9ccc3694cd7cce7c47e1367545e810c2fd64
GitHub Action <action@github.com>
2022-10-18 20:33:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/adamwolf/runaftertests/services/MyProjectService.kt
+++ src/main/kotlin/com/github/adamwolf/runaftertests/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.adamwolf.runaftertests.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.adamwolf.runaftertests.MyBundle
 
 class MyProjectService(project: Project) {
 
